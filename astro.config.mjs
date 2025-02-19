06fc7fcb5cda56abb7cdef3cb81203af448e5d90
maeda <maeda.ibuki554@mail.kyutech.jp>
2025-02-19 13:24:27 +0900

    google analytics 追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,22 @@ import sitemap from '@astrojs/sitemap';
 
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import partytown from "@astrojs/partytown";
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ibukimaeda.github.io',
-	integrations: [mdx(), sitemap()],
+    integrations: [
+        mdx(),
+        sitemap(),
+        partytown({
+            // Adds dataLayer.push as a forwarding-event.
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ],
 	markdown: {
 		remarkPlugins: [remarkMath],
 		rehypePlugins: [rehypeKatex],
