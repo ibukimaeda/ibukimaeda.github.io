754a810e7163f3b5db83f5fbadfa18905e7cf474
maeda <maeda.ibuki554@mail.kyutech.jp>
2025-02-19 11:58:03 +0900

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+
 import sitemap from '@astrojs/sitemap';
+
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ibukimaeda.github.io',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	}
 });
